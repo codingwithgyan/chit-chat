3041577d403f289878c0c9321b7389fbff59dcb4
Gyaneshwar Kumar <codewithgyan@gmail.com>
2022-02-26 00:58:24 +0530

    minor config

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -2,7 +2,7 @@ import { Children, createContext, useState } from "react"
 import { io } from "socket.io-client";
 
 export const SocketContext = createContext();
-const socket = io("http://localhost:2345");
+const socket = io("/");
 export function SocketContextProvider({children})
 {
     const [onlineUsers,setOnlineUsers]=useState([]);
